9b53ef5909e7938e6ae870a9a756fd800f91b03d
Echeverra <994435779@qq.com>
2018-06-11 22:27:05 +0800

    base controller for use login on other page when not login

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -5,5 +5,10 @@ use think\Controller;
 
 class Base extends Controller
 {
-
+    public function _initialize()
+    {
+        if(!session('username') || !session('id')) {
+            $this->error('请先登录', 'login/login');
+        }
+    }
 }
